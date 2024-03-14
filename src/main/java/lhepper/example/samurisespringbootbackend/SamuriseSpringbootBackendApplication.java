32e622ab67d19254d7b0fab0fc8eeb6c9c3b0124
l-hepper <liamhepper1@gmail.com>
2024-03-14 16:39:31 +0000

    initial repo setup

---
+++ src/main/java/lhepper/example/samurisespringbootbackend/SamuriseSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package lhepper.example.samurisespringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuriseSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuriseSpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lhepper/example/samurisespringbootbackend/SamuriseSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lhepper.example.samurisespringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamuriseSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
